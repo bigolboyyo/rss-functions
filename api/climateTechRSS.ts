9c68ed2becb9dd5663c2ec233d6f407eb4102484
bigolboyyo <wesley.grant94@gmail.com>
2023-06-04 19:18:15 -0400

    rss namespace

--- api/climateTechRSS.ts
+++ api/climateTechRSS.ts
@@ -2,6 +2,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { RSSConfig } from '../data/RSSConfig';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  const config = new RSSConfig();
-  await config.handleRequest(req, res);
+  const rss = new RSSConfig();
+  await rss.handleRequest(req, res);
 }
