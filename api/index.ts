896324f49fc83cee47f3b9dd7480987e1063fc89
bigolboyyo <wesley.grant94@gmail.com>
2023-06-06 12:45:21 -0400

    Auto readme to index.js

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,25 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import fs from 'fs';
+import path from 'path';
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  
+  const readmePath = path.join(__dirname, '..', 'README.md');
+  const readmeContent = fs.readFileSync(readmePath, 'utf8');
+
   const html = `
     <html>
-      <body style="display: flex; flex-direction: column; justify-content: center;">
-        <dialog style="display: flex; flex-direction: column;">
+      <body>
+        <div style="display: flex; flex-direction: column; justify-content: center; max-width: 100vw;">
           <h1>Welcome to the Climate Tech RSS Bridge!</h1>
           <p>Endpoints:</p>
           <ul>
             <li><a href="api/climateTechRSS">/climateTechRSS</a></li>
           </ul>
-        </dialog>
+          <div>
+            <h2>README.md:</h2>
+            <pre style="text-wrap: wrap !important;">${readmeContent}</pre>
+          </div>
+        </div>
       </body>
     </html>
   `;
